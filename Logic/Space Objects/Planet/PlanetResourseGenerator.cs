e76a8c9897370fad4204967829c93ef88c9d4264
vind113 <dima.yarmoshchuk@gmail.com>
2019-01-08 12:40:39 +0200

    Доработан класс PlanetResourceGenerator.

--- Logic/Space Objects/Planet/PlanetResourseGenerator.cs
+++ Logic/Space Objects/Planet/PlanetResourseGenerator.cs
@@ -1,38 +1,75 @@
-﻿using Logic.Resource;
+﻿using System;
+using Logic.Resource;
+using Logic.SupportClasses;
 
 namespace Logic.SpaceObjects.PlanetClasses {
     public class PlanetResourceGenerator {
+        private const double MASS_OF_TEN_KM_CRUST = 10d * ((3d) * 10E9);
+
+        private const int FRACTION_OF_HYDROGEN = 200;
+        private const double GAS_GIANT_HYDROGEN_MULTIPLIER = 1.4 * 20 * 1E3 * 1E9;
+
+        private const int FRACTION_OF_COMMON_METALS_TERRA = 20;
+        private const int FRACTION_OF_COMMON_METALS_FERRIA = 2;
+
+        private const double FRACTION_OF_RARE_METALS_TERRA = 1E5;
+        private const double FRACTION_OF_RARE_METALS_FERRIA = 1E4;
+
         public PlanetResourceGenerator() {
 
         }
 
         public Resources GenerateFor(Planet planet) {
-            double commonMetals = 0;
-            double rareEarthElements = 0;
-            double hydrogen = 0;
-
-            double massOfTenKmCrust = (10d * ((3d) * 10E9));
-
-            if (planet.Type.SubstancesClass == SubstancesClass.Terra) {
-                commonMetals = planet.Area * (massOfTenKmCrust / 20);
-                rareEarthElements = planet.Area * (massOfTenKmCrust / 1E5);
-                hydrogen = planet.Area * (massOfTenKmCrust / 200);
-
+            switch (planet.Type.SubstancesClass) {
+                case SubstancesClass.Ferria:
+                    return GenerateForFerria(planet.Area);
+                case SubstancesClass.Terra:
+                    return GenerateForTerra(planet.Area);
+                case SubstancesClass.Jupiter:
+                    return GenerateForJupiter(planet.Area);
+                default:
+                    throw new ArgumentException("Incorect substance type");
             }
-            else if (planet.Type.SubstancesClass == SubstancesClass.Ferria) {
-                commonMetals = planet.Area * (massOfTenKmCrust / 2);
-                rareEarthElements = planet.Area * (massOfTenKmCrust / 1E4);
-                hydrogen = planet.Area * (massOfTenKmCrust / 200);
+        }
 
-            }
-            else if (planet.Type.SubstancesClass == SubstancesClass.Jupiter) {
-                commonMetals = 0;
-                rareEarthElements = 0;
-                hydrogen = planet.Area * (1.4 * 20 * 1E3 * 1E9);
+        private static Resources GenerateForTerra(double planetArea) {
+            double randomResourceMultiplier = GetRandomResourceMultiplier();
 
-            }
+            double hydrogen = 
+                planetArea * (MASS_OF_TEN_KM_CRUST / FRACTION_OF_HYDROGEN) * randomResourceMultiplier;
+            double commonMetals = 
+                planetArea * (MASS_OF_TEN_KM_CRUST / FRACTION_OF_COMMON_METALS_TERRA) * randomResourceMultiplier;
+            double rareEarthElements = 
+                planetArea * (MASS_OF_TEN_KM_CRUST / FRACTION_OF_RARE_METALS_TERRA) * randomResourceMultiplier;
 
             return new Resources(hydrogen, commonMetals, rareEarthElements);
+        }
+
+        private static Resources GenerateForFerria(double planetArea) {
+            double randomResourceMultiplier = GetRandomResourceMultiplier();
+
+            double hydrogen = 
+                planetArea * (MASS_OF_TEN_KM_CRUST / FRACTION_OF_HYDROGEN) * randomResourceMultiplier;
+            double commonMetals = 
+                planetArea * (MASS_OF_TEN_KM_CRUST / FRACTION_OF_COMMON_METALS_FERRIA) * randomResourceMultiplier;
+            double rareEarthElements = 
+                planetArea * (MASS_OF_TEN_KM_CRUST / FRACTION_OF_RARE_METALS_FERRIA) * randomResourceMultiplier;
+            
+            return new Resources(hydrogen, commonMetals, rareEarthElements);
+        }
+
+        private static Resources GenerateForJupiter(double planetArea) {
+            double commonMetals = 0;
+            double rareEarthElements = 0;
+
+            double randomResourceMultiplier = GetRandomResourceMultiplier();
+            double hydrogen = planetArea * GAS_GIANT_HYDROGEN_MULTIPLIER * randomResourceMultiplier;
+
+            return new Resources(hydrogen, commonMetals, rareEarthElements);
+        }
+
+        private static double GetRandomResourceMultiplier() {
+            return HelperRandomFunctions.GetRandomDouble() * 2;
         }
     }
 }
